7e86b7428ed7d5041c49075f1fcb9fe72e5758c2
Aaron Turner <aaron@aaronthedev.com>
2016-08-27 23:44:32 +0000

    Added the iris hellow world program

---
+++ main.go
@@ -0,0 +1,15 @@
+//Starting "hello world" iris server
+
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+    api := iris.New()
+    api.Get("/hi", hi)
+    api.Listen(":3000")
+}
+
+func hi(ctx *iris.Context){
+   ctx.Write("Hi %s", "iris")
+}
