eda8af3c6428970aa4ff6a6df96cdd5d3b600938
Aaron Turner <aaron@aaronthedev.com>
2016-08-29 00:06:42 +0000

    Started learning some more iris, got a working post request

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 //Starting "hello world" iris server
+//See here for how context works: https://kataras.gitbooks.io/iris/content/context.html
 
 package main
 
 import "github.com/kataras/iris"
 
 func main() {
+    //Initialize our api
     api := iris.New()
-    api.Get("/hi", hi)
+
+    api.Get("/", defaultRoute)
+    api.Get("/hi", hiGet)
+    api.Post("/speak", speakPost)
     api.Listen(":4000")
 }
 
-func hi(ctx *iris.Context){
+//DefaultRoute
+func defaultRoute(ctx *iris.Context) {
+   ctx.Write("Welcome to the torch2424 goSmartHome!")
+}
+
+//The /hi route GET
+func hiGet(ctx *iris.Context){
    ctx.Write("Hi %s", "iris")
+}
+
+//The /speak Post
+func speakPost(ctx *iris.Context) {
+    testField := ctx.FormValueString("test")
+	// myDb.InsertUser(...)
+	println(testField)
+	println("Post from /speak")
 }
