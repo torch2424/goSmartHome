51651d945079235c1a6c9c7610365492dfd2a932
Aaron Turner <aaron@aaronthedev.com>
2017-04-02 14:18:15 -0700

    Testing 1st of the month, but I think it is done

--- hourlyjobs/hourlyjobs.go
+++ hourlyjobs/hourlyjobs.go
@@ -4,6 +4,7 @@ package hourlyjobs
 
 import "fmt"
 import "time"
+import "os/exec"
 
 var timeTicker *time.Ticker
 
@@ -19,13 +20,18 @@ func HourlyJobs() {
     for _ = range timeTicker.C {
 
       // Get our current time
+			//Month 1-12
+			// Hour 1 - 24 (army time)
       var month int = int(time.Now().Month())
+			var dayOfMonth int = int(time.Now().Day())
       var hour int = int(time.Now().Hour())
-      fmt.Println(month)
-      fmt.Println(hour)
 
       // if it is 10AM on the 1st of the month
-
+			if hour == 2 && dayOfMonth == 2 {
+				fmt.Println("It's the 1st of tha month!!!")
+				//Play 1st of the month
+				exec.Command("aplay", "assets/1stOfThaMonth.mp3")
+			}
     }
   }()
 }
