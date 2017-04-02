b36ac3b17fcaf5e244466dca162e520a5165d1e0
Aaron Turner <aaron@aaronthedev.com>
2017-04-02 13:42:16 -0700

    Added some testing println

--- hourlyjobs/hourlyjobs.go
+++ hourlyjobs/hourlyjobs.go
@@ -21,6 +21,8 @@ func HourlyJobs() {
       // Get our current time
       var month int = int(time.Now().Month)
       var hour int = int(time.Now().Hour)
+      fmt.Println(month)
+      fmt.Println(hour)
 
       // if it is 10AM on the 1st of the month
 
