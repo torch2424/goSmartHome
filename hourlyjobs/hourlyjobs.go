1d7b4fe8a03a052834dd0ba3a5a949cb46ee9b66
Aaron Turner <aaron@aaronthedev.com>
2017-04-02 14:24:59 -0700

    Finished 1st of the month playing

--- hourlyjobs/hourlyjobs.go
+++ hourlyjobs/hourlyjobs.go
@@ -22,12 +22,12 @@ func HourlyJobs() {
       // Get our current time
 			//Month 1-12
 			// Hour 1 - 24 (army time)
-      var month int = int(time.Now().Month())
+      //var month int = int(time.Now().Month())
 			var dayOfMonth int = int(time.Now().Day())
       var hour int = int(time.Now().Hour())
 
       // if it is 10AM on the 1st of the month
-			if hour == 2 && dayOfMonth == 2 {
+			if hour == 10 && dayOfMonth == 1 {
 				fmt.Println("It's the 1st of tha month!!!")
 				//Play 1st of the month
 				exec.Command("aplay", "assets/1stOfThaMonth.mp3")
